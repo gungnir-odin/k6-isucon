bbc3ecf4d6c2ef11728b222bf7637497752b842c
naoki.wakou <naoki.wakou@gmail.com>
2024-07-25 23:22:27 +0900

    url関数のimport元を修正

--- initialize.js
+++ initialize.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { sleep } from "k6";
-import { url } from "./config";
+import { url } from "./config.js";
 
 export default function (){
     http.get(url("/initialize"), {
