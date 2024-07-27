a463ca424156bcbfd701872c1972298b24b31194
naoki.wakou <naoki.wakou@gmail.com>
2024-07-27 22:50:23 +0900

    アカウント情報の保持をjsonファイルで行うように修正

--- comment.js
+++ comment.js
@@ -2,11 +2,13 @@ import http from "k6/http";
 import { check } from "k6";
 import { parseHTML } from "k6/html";
 import { url } from "./config.js";
+import { getAccount } from "./accounts.js";
 
 export default function () {
+    const account = getAccount();
     const login_res = http.post(url("/login"), {
-        account_name: 'terra',
-        password: 'tettaterra',
+        account_name: account.account_name,
+        password: account.password,
     });
 
     check(login_res, {
--- postimage.js
+++ postimage.js
@@ -1,13 +1,16 @@
 import http from "k6/http";
 import { parseHTML } from "k6/html";
 import { url } from "./config.js";
+import { getAccount } from "./accounts.js";
 
 const testImage = open("testimage.jpg", "b");
 
 export default function () {
+    const account = getAccount();
+
     const res = http.post(url("/login"), {
-        account_name: "terra",
-        password: "terraterra",
+        account_name: account.account_name,
+        password: account.password,
     });
     const doc = parseHTML(res.body);
     const token = doc.find('input[name="csrf_token"]').first().attr("value");
